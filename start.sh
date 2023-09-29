67653522a9bd6c9ed1380e813caafbb4434687c5
Ghostzinn07 <99512155+Ghostzinn07@users.noreply.github.com>
2023-09-29 16:16:35 -0300

    Atualizar o start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,26 @@ if ! pip show instagram_private_api &> /dev/null; then
     echo "Instalando a dependência 'instagram_private_api'..."
     pip install instagram_private_api
 else
-echo "Verificando as dependências..."
+    echo "Verificando as dependências..."
 fi
 
 if ! pip show colorama &> /dev/null; then
     echo "Instalando a dependência 'colorama'..."
     pip install colorama
 else
-echo "Verificando as dependências..."
+    echo "Verificando as dependências..."
 fi
 
-python main.py
+echo "Escolha uma opção:"
+echo "1 - Ganhar seguidores."
+echo "2 - Parar de seguir celebridades."
+
+read choice
+
+if [ $choice -eq 1 ]; then
+    python main.py
+elif [ $choice -eq 2 ]; then
+    python stop.py
+else
+    echo "Opção inválida."
+fi
