b148342a829befce67fddd3ab796bb196aee4f0f
Ghostzinn07 <99512155+Ghostzinn07@users.noreply.github.com>
2023-10-02 01:18:35 -0300

    Atualizar o automatic.py

--- automatic.py
+++ automatic.py
@@ -5,6 +5,7 @@ import time
 import random
 import os
 from colorama import Fore, Back, Style, init
+
 loader = instaloader.Instaloader()
 init(autoreset=True)
 
@@ -33,37 +34,38 @@ def main():
             user_id = user_info['user']['pk']
             profile = instaloader.Profile.from_username(loader.context, perfil_alvo)
             num_publicacoes = profile.mediacount
+
             if num_publicacoes > total_postagens_anterior:
-            total_postagens_anterior = num_publicacoes
-            api = Client(username, password)
-            if user_id:
-                num_posts = user_info['user']['media_count']
-                num_posts_to_check = min(5, num_posts)
+                total_postagens_anterior = num_publicacoes
 
-                if num_posts_to_check > 0:
-                    user_feed = api.user_feed(user_id)
-                    recent_posts = user_feed.get("items", [])[:num_posts_to_check]
+                if user_id:
+                    num_posts = user_info['user']['media_count']
+                    num_posts_to_check = min(5, num_posts)
 
-                    if recent_posts:
-                        recent_posts.sort(key=lambda x: x['taken_at'], reverse=True)
-                        latest_post = recent_posts[0]
+                    if num_posts_to_check > 0:
+                        user_feed = api.user_feed(user_id)
+                        recent_posts = user_feed.get("items", [])[:num_posts_to_check]
 
-                        media_id = latest_post['id']
+                        if recent_posts:
+                            recent_posts.sort(key=lambda x: x['taken_at'], reverse=True)
+                            latest_post = recent_posts[0]
 
-                        if media_id not in postagens_comentadas:
-                            comentarios = [comentario1, comentario2, comentario3]
-                            comentario_escolhido = random.choice(comentarios)
+                            media_id = latest_post['id']
 
-                            if api.post_comment(media_id, comentario_escolhido):
-                                print_green(f"Comentado no post mais recente de {perfil_alvo}: {comentario_escolhido}")
-                                postagens_comentadas.append(media_id)
-                                time.sleep(3)
-                            else:
-                                print_yellow(f"Não foi possível comentar no post de {perfil_alvo}")
+                            if media_id not in postagens_comentadas:
+                                comentarios = [comentario1, comentario2, comentario3]
+                                comentario_escolhido = random.choice(comentarios)
 
-                time.sleep(10)
-            else:
-                print_red(f"Usuário {perfil_alvo} não encontrado")
+                                if api.post_comment(media_id, comentario_escolhido):
+                                    print_green(f"Comentado no post mais recente de {perfil_alvo}: {comentario_escolhido}")
+                                    postagens_comentadas.append(media_id)
+                                    time.sleep(3)
+                                else:
+                                    print_yellow(f"Não foi possível comentar no post de {perfil_alvo}")
+
+                    time.sleep(10)
+                else:
+                    print_red(f"Usuário {perfil_alvo} não encontrado")
 
     except Exception as e:
         print_red(f"Erro geral: {str(e)}")
